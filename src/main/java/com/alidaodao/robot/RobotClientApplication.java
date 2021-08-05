7c64b2f3961521edabc59a6dc69da79305d4a80a
bosong <songbo2021@outlook.com>
2021-08-05 11:40:58 +0800

    first commit

---
+++ src/main/java/com/alidaodao/robot/RobotClientApplication.java
@@ -0,0 +1,15 @@
+package com.alidaodao.robot;
+
+
+/**
+ * robot test application
+ *
+ */
+public class RobotClientApplication {
+
+    public static void main(String[] args) {
+        System.out.println("robot application");
+    }
+
+
+}
---
+++ src/test/java/com/alidaodao/robot/RobotClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alidaodao.robot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
